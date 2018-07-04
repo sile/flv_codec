7a28b3283388d499b7cfa9b0c36d1bd9c0eb7c8f
Takeru Ohta <phjgt308@gmail.com>
2018-07-05 00:18:44 +0900

    Update bytecodec to v0.4.x

--- src/header.rs
+++ src/header.rs
@@ -1,6 +1,7 @@
-use bytecodec::{ByteCount, Decode, Encode, Eos, ExactBytesEncode, Result};
 use bytecodec::bytes::CopyableBytesDecoder;
-use bytecodec::combinator::{Buffered, Length, SkipRemaining};
+use bytecodec::combinator::Length;
+use bytecodec::padding::PaddingDecoder;
+use bytecodec::{ByteCount, Decode, Encode, Eos, Result, SizedEncode};
 
 #[derive(Debug, Default, Clone)]
 pub struct FlvHeader {
@@ -10,31 +11,35 @@ pub struct FlvHeader {
 
 #[derive(Debug, Default)]
 pub struct FlvHeaderDecoder {
-    bytes: Buffered<CopyableBytesDecoder<[u8; 9]>>,
-    rest: Length<SkipRemaining<NullDecoder>>,
+    bytes: CopyableBytesDecoder<[u8; 9]>,
+    padding: Length<PaddingDecoder>,
 }
 impl Decode for FlvHeaderDecoder {
     type Item = FlvHeader;
 
-    fn decode(&mut self, buf: &[u8], eos: Eos) -> Result<(usize, Option<Self::Item>)> {
-        let mut offset = 0;
-        if !self.bytes.has_item() {
-            offset += track!(self.bytes.decode(buf, eos))?.0;
-            if !self.bytes.has_item() {
-                return Ok((offset, None));
-            }
-        }
+    fn decode(&mut self, buf: &[u8], eos: Eos) -> Result<usize> {
+        // let mut offset = 0;
+        // if !self.bytes.has_item() {
+        //     offset += track!(self.bytes.decode(buf, eos))?.0;
+        //     if !self.bytes.has_item() {
+        //         return Ok((offset, None));
+        //     }
+        // }
         unimplemented!()
     }
 
-    fn has_terminated(&self) -> bool {
-        false
+    fn finish_decoding(&mut self) -> Result<Self::Item> {
+        unimplemented!()
+    }
+
+    fn is_idle(&self) -> bool {
+        unimplemented!()
     }
 
     fn requiring_bytes(&self) -> ByteCount {
         self.bytes
             .requiring_bytes()
-            .add_for_decoding(self.rest.requiring_bytes())
+            .add_for_decoding(self.padding.requiring_bytes())
     }
 }
 
@@ -59,27 +64,8 @@ impl Encode for FlvHeaderEncoder {
         unimplemented!()
     }
 }
-impl ExactBytesEncode for FlvHeaderEncoder {
+impl SizedEncode for FlvHeaderEncoder {
     fn exact_requiring_bytes(&self) -> u64 {
         unimplemented!()
-    }
-}
-
-// TODO: move to bytecodec
-#[derive(Debug, Default)]
-struct NullDecoder;
-impl Decode for NullDecoder {
-    type Item = ();
-
-    fn decode(&mut self, _buf: &[u8], _eos: Eos) -> Result<(usize, Option<Self::Item>)> {
-        Ok((0, Some(())))
-    }
-
-    fn has_terminated(&self) -> bool {
-        false
-    }
-
-    fn requiring_bytes(&self) -> ByteCount {
-        ByteCount::Finite(0)
     }
 }
